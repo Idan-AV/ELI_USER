85afd0d88cf2d426e6d7e3ca396199fe0dda1a97
avulov idan <avulovidan@gmail.com>
2025-06-03 14:31:28 +0300

    Initial commit

---
+++ demo/src/main/java/com/example/demo/exception/AlreadyExists.java
@@ -0,0 +1,13 @@
+package com.example.demo.exception;
+
+// AlreadyExists.java
+
+/**
+ * Exception thrown when a resource already exists in the system.
+ * For example, when trying to add a student with an email that already exists.
+ */
+public class AlreadyExists extends RuntimeException {
+    public AlreadyExists(String message) {
+        super(message);
+    }
+}
